938bab1fa6fe815b6d14a76d961d78f1f6ce8668
Devansh27112001 <dkansara2711@gmail.com>
2026-01-22 00:09:33 -0500

    product-service added

---
+++ product-service/src/main/java/com/app/productservice/controller/ProductController.java
@@ -0,0 +1,58 @@
+package com.app.productservice.controller;
+
+import com.app.productservice.dto.ProductRequest;
+import com.app.productservice.dto.ProductResponse;
+import com.app.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("")
+    public ResponseEntity<List<ProductResponse>> findAll(){
+        return ResponseEntity.ok(productService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> findById(@PathVariable Long id){
+        return productService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("")
+    public ResponseEntity<ProductResponse> create(@RequestBody ProductRequest productRequest){
+        System.out.println("ProductController:create");
+        return new ResponseEntity<>(productService.addProduct(productRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> update(
+            @RequestBody ProductRequest productRequest,
+            @PathVariable Long id){
+        return productService.updateProduct(productRequest,id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() ->  ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        boolean deleted = productService.deleteById(id);
+        return deleted ? ResponseEntity.noContent().build() :  ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<ProductResponse>> searchByKeyWord(@RequestParam String keyword){
+        return ResponseEntity.ok(productService.searchProduct(keyword));
+    }
+
+}
---
+++ product-service/src/main/java/com/app/productservice/service/ProductService.java
@@ -0,0 +1,85 @@
+package com.app.productservice.service;
+
+import com.app.productservice.repository.ProductRepo;
+import com.app.productservice.dto.ProductRequest;
+import com.app.productservice.dto.ProductResponse;
+import com.app.productservice.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public List<ProductResponse> findAll(){
+        return productRepo.findByActiveTrue().stream().map(this::mapToProductResponse).toList();
+    }
+
+    public Optional<ProductResponse> findById(Long id){
+        return productRepo.findById(id).map(this::mapToProductResponse);
+    }
+
+    public ProductResponse addProduct(ProductRequest  productRequest){
+        Product product = new Product();
+        // Converting ProductRequest to Product
+        mapProductRequestToProduct(product, productRequest);
+
+        // Saving the Product to the database
+        Product savedProduct = productRepo.save(product);
+
+        // Mapping the saved product returned from the database to ProductResponse.
+        return mapToProductResponse(savedProduct);
+    }
+
+    // UTIL method
+    private ProductResponse mapToProductResponse(Product product) {
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setId(product.getId());
+        productResponse.setName(product.getName());
+        productResponse.setCategory(product.getCategory());
+        productResponse.setDescription(product.getDescription());
+        productResponse.setImageUrl(product.getImageUrl());
+        productResponse.setPrice(product.getPrice());
+        productResponse.setStockQuantity(product.getStockQuantity());
+        productResponse.setActive(product.getActive());
+        return productResponse;
+    }
+
+    // UTIL method
+    private void mapProductRequestToProduct(Product product, ProductRequest productRequest){
+        product.setName(productRequest.getName());
+        product.setCategory(productRequest.getCategory());
+        product.setDescription(productRequest.getDescription());
+        product.setPrice(productRequest.getPrice());
+        product.setImageUrl(productRequest.getImageUrl());
+        product.setStockQuantity(productRequest.getStockQuantity());
+    }
+
+    public Optional<ProductResponse> updateProduct(ProductRequest productRequest, Long id) {
+        return productRepo.findById(id).map(existing -> {
+            // Mapping the ProductRequest to Product.
+            mapProductRequestToProduct(existing, productRequest);
+            Product savedProduct = productRepo.save(existing);
+            return mapToProductResponse(savedProduct);
+        });
+    }
+
+    public boolean deleteById(Long id) {
+        // Soft delete: Changing ACTIVE status to false.
+        return productRepo.findById(id)
+                .map(existingProduct -> {
+                    existingProduct.setActive(false);
+                    productRepo.save(existingProduct);
+                    return true;
+                }).orElse(false);
+    }
+
+    public List<ProductResponse> searchProduct(String keyword) {
+        return productRepo.searchProducts(keyword).stream().map(this::mapToProductResponse).toList();
+    }
+}
